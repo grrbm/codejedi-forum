c351b208781a52a6d50f050fd3f3fa5ad711d601
Guilherme Reis <grrbm2@gmail.com>
2021-10-16 00:03:11 -0300

    added babel-plugin-transform-runtime

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { store, persistor } from "../store";
-//import Routes from "../routes";
 
 ReactDOM.render(
   <Provider store={store}>
